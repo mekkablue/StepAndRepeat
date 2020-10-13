bfcdd2c597e517f24b996e847e8f5afe441aa68b
Rainer Scheichelbauer <res@glyphsapp.com>
2020-10-13 18:40:31 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# StepAndRepeat
-Glyphs.app Palette for repeating shapes
+# Step and Repeat
+
+ Palette plug-in for the Glyphs.app font editor. Allows shift-repeating shapes.
+
+![Step and Repeat](StepAndRepeat.png)
+
+*After installation, Step and Repeat will appear in the app palette on the right side (Cmd-Opt-P).*
+
+### Usage
+
+1. Select one or more objects.
+2. Type (or step with arrows, Shift-arrows, or Cmd-Shift-arrows) an *X* shift, a *Y* shift.
+3. Type (or step with arrows, Shift-arrows, or Cmd-Shift-arrows) a *Steps* number, including the original. Minimum: 2.
+4. Press the *Go* button.
+
+The newly created objects will be added to the selection for making further transformations possible.
+
+### Example
+
+You have a circle with a radius of 80 units, and you want a grid of 5×5 circles with 20 units between adjacent circles.
+
+1. Select the circle by double clicking one of its segments with the Select tool (shortcut V).
+2. Type *X* 100, *Y* 0, *Steps* 5, and press *Go.*
+3. Type *X* 0, *Y* 100, *Steps* 5, and press *Go.*
+
+### Installation
+
+1. Open *Window > Plugin Manager*
+2. Find *Step and Repeat* and click the *Install* button
+3. Restart Glyphs.app
+
+### License
+
+Copyright 2020 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ StepAndRepeat.glyphsPalette/Contents/Resources/plugin.py
@@ -0,0 +1,156 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	Palette Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+import math
+from copy import copy as copy
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+from AppKit import NSAffineTransform, NSAffineTransformStruct
+
+def transform(shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+	"""
+	Returns an NSAffineTransform object for transforming layers.
+	Apply an NSAffineTransform t object like this:
+		Layer.transform_checkForSelection_doComponents_(t,False,True)
+	Access its transformation matrix like this:
+		tMatrix = t.transformStruct() # returns the 6-float tuple
+	Apply the matrix tuple like this:
+		Layer.applyTransform(tMatrix)
+		Component.applyTransform(tMatrix)
+		Path.applyTransform(tMatrix)
+	Chain multiple NSAffineTransform objects t1, t2 like this:
+		t1.appendTransform_(t2)
+	"""
+	myTransform = NSAffineTransform.transform()
+	if rotate:
+		myTransform.rotateByDegrees_(rotate)
+	if scale != 1.0:
+		myTransform.scaleBy_(scale)
+	if not (shiftX == 0.0 and shiftY == 0.0):
+		myTransform.translateXBy_yBy_(shiftX,shiftY)
+	if skew:
+		skewStruct = NSAffineTransformStruct()
+		skewStruct.m11 = 1.0
+		skewStruct.m22 = 1.0
+		skewStruct.m21 = math.tan(math.radians(skew))
+		skewTransform = NSAffineTransform.transform()
+		skewTransform.setTransformStruct_(skewStruct)
+		myTransform.appendTransform_(skewTransform)
+	return myTransform
+
+class StepAndRepeat(PalettePlugin):
+	
+	dialog = objc.IBOutlet()
+	xField = objc.IBOutlet()
+	yField = objc.IBOutlet()
+	stepsField = objc.IBOutlet()
+
+	@objc.python_method
+	def settings(self):
+		self.name = Glyphs.localize({
+			'en': 'Step & Repeat',
+			'de': 'Wiederholen',
+			'fr': 'Répéter',
+			'es': 'Repetir',
+			'pt': 'Repetir',
+			})
+		
+		# Load .nib dialog (without .extension)
+		self.loadNib('IBdialog', __file__)
+
+	@objc.python_method
+	def start(self):
+		Glyphs.registerDefault('com.mekkablue.StepAndRepeat.x', 0.0)
+		Glyphs.registerDefault('com.mekkablue.StepAndRepeat.y', 50.0)
+		Glyphs.registerDefault('com.mekkablue.StepAndRepeat.steps', 5)
+		self.xField.setStringValue_(Glyphs.defaults['com.mekkablue.StepAndRepeat.x'])
+		self.yField.setStringValue_(Glyphs.defaults['com.mekkablue.StepAndRepeat.y'])
+		self.stepsField.setStringValue_(Glyphs.defaults['com.mekkablue.StepAndRepeat.steps'])
+
+	# Action triggered by UI
+	@objc.IBAction
+	def setXValue_( self, sender ):
+		
+		# Store value coming in from dialog
+		Glyphs.defaults['com.mekkablue.StepAndRepeat.x'] = sender.floatValue()
+		
+	# Action triggered by UI
+	@objc.IBAction
+	def setYValue_( self, sender ):
+		
+		# Store value coming in from dialog
+		Glyphs.defaults['com.mekkablue.StepAndRepeat.y'] = sender.floatValue()
+		
+	# Action triggered by UI
+	@objc.IBAction
+	def setStepsValue_( self, sender ):
+		
+		# Store value coming in from dialog
+		Glyphs.defaults['com.mekkablue.StepAndRepeat.steps'] = sender.intValue()
+
+	@objc.IBAction
+	def stepAndRepeat_(self, sender):
+		# Extract font from sender
+		font = Glyphs.font
+
+		# We’re in the Edit View
+		if font and font.currentTab:
+
+			# Check whether glyph is open and there is a selection:
+			if len(font.selectedLayers) == 1:
+				layer = font.selectedLayers[0]
+				if layer and layer.selection:
+					
+					stepX = Glyphs.defaults["com.mekkablue.StepAndRepeat.x"]
+					stepY = Glyphs.defaults["com.mekkablue.StepAndRepeat.y"]
+					times = Glyphs.defaults["com.mekkablue.StepAndRepeat.steps"]
+					
+					repeatingShapes = []
+					
+					if (stepX != 0.0 or stepY != 0.0) and times > 1:
+						for item in layer.selection:
+							if type(item) == GSNode:
+								path = item.parent
+								if not path in repeatingShapes:
+									repeatingShapes.append(path)
+							elif type(item) != GSAnchor:
+								if not item in repeatingShapes:
+									repeatingShapes.append(item)
+						if repeatingShapes:
+							for i in range(1,times):
+								shiftMatrix = transform(
+									shiftX=stepX*(i),
+									shiftY=stepY*(i)
+									).transformStruct()
+								for shape in repeatingShapes:
+									repeatedShape = copy(shape)
+									repeatedShape.applyTransform(shiftMatrix)
+									layer.shapes.append(repeatedShape)
+									if type(repeatedShape) == GSPath:
+										for node in repeatedShape.nodes:
+											layer.selection.append(node)
+									else:
+										layer.selection.append(repeatedShape)
+					else:
+						Message(
+							title="Step & Repeat Error",
+							message="The current values make no sense. X and Y cannot both be zero, and Steps must be at least 2. You need to tab out of the field or press the Return key to confirm an entry.",
+							OKButton=None
+							)
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
